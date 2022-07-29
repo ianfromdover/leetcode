9b6d19420c7d2254ad9a8065555523156daf62e4
ian-from-dover <ian09hong@gmail.com>
2022-07-29 15:15:01 +0800

    1672: LINQ One-liner

--- 1672. Richest Customer Wealth/max.cs
+++ 1672. Richest Customer Wealth/max.cs
@@ -1,8 +1,3 @@
 public class Solution {
-    public int MaximumWealth(int[][] accounts) {
-        int a = 1;
-        foreach (var e in accounts)
-            a = Math.Max(a, e.Sum());
-        return a;
-    }
+    public int MaximumWealth(int[][] accounts) => accounts.Max(customer => customer.Sum());
 }
